e1a479bfd7db236a095c5b173ac5a65f92d76281
Strli <18792146966@163.com>
2019-03-19 17:29:11 +0800

    update:分别使用三种库解析返回数据,改进编码不规范问题

--- requests/CrawUnivRank.py
+++ requests/CrawUnivRank.py
@@ -1,50 +1,79 @@
 #CrawUnivRank.py
 #-*- coding:utf-8 -*-
-import requests
-from bs4 import BeautifulSoup
-import bs4
 import time
 import csv
+import logging
+
+import requests
+
+from bs4 import BeautifulSoup
+from lxml import etree
+from pyquery import PyQuery as pq
+
+logging.basicConfig(level=logging.INFO)
 
 #get data
-def getHTML(url):
-	try:
-		rq = requests.get(url,timeout=30)
-		rq.raise_for_status()
-		rq.encoding = rq.apparent_encoding
-		print(rq.apparent_encoding)
-		return rq.text
-	except:
-		print("抓取失败！") 
-		return " "
+def get_html(url):
+    try:
+        rq = requests.get(url,timeout=30)
+        rq.raise_for_status()
+        rq.encoding = rq.apparent_encoding
+        return rq.text
+    except:
+        print("抓取失败！") 
+        return " "
 
 #handle the data
-def fillUnivrList(html,ulist):
-	soup = BeautifulSoup(html,"html.parser")
-	for tr in soup.find('tbody').children:
-		if isinstance(tr,bs4.element.Tag):
-			tds = tr("td")
-			ulist.append([tds[0].string,tds[1].string,tds[2].string,tds[3].string])
-
+def fill_univrList_soup(html, ulist):
+    """使用beautifulsoup库进行解析"""
+    
+    soup = BeautifulSoup(html, "html.parser")
+    for tr in soup.find('tbody').children:
+        if isinstance(tr, bs4.element.Tag):
+            tds = tr("td")
+            ulist.append([tds[0].string, tds[1].string, tds[2].string, 
+                tds[3].string])
+                
+def fill_univrList_pq(response, ulist):
+    '''使用pyquery库进行解析'''
+    html = pq(response)
+    results = html('.hidden_zhpm tr').items()
+    print(results)
+    for tr in results:
+        #tds = tr('td').text() 如果不考虑逐项存入表格的话，可以直接试用text（）输出一行文本文字 
+        tds= tr('td')
+        td_list = []
+        for td in tds.items():
+            td_list.append(td.text())
+        ulist.append([td_list[0], td_list[1], td_list[2], td_list[3]])
+        
+def fill_univrList_xpath(response, ulist):
+    '''使用xpath库进行解析'''
+    html = etree.HTML(response)
+    results = html.xpath('//tr[@class="alt"]')
+    for tr in results:
+        #'.'表示当前节点，xpath方法默认从整个html文档中查找，无论前面调用该方法的对象是什么标签。否则会从整个html文档中重新查找
+        tds = tr.xpath('./td//text()') 
+        ulist.append(tds[:4])
+        
 #foramt data && data store 
-def printUniverList(ulist,num):
-	tplt = "{0:^10}\t{1:{4}^10}\t{2:{4}^10}\t{3:^10}"
-	# print(tplt.format("排名","大学名称","省市","排名积分",chr(12288)))
+def print_univerList(ulist, num):
+    tplt = "{0:^10}\t{1:{4}^10}\t{2:{4}^10}\t{3:^10}"
+    # print(tplt.format("排名","大学名称","省市","总分",chr(12288)))
+    
+    with open('./tUniverList.csv','w+',newline='') as f: 
+        csvWriter = csv.writer(f)
+        csvWriter.writerow(["排名","大学名称","省市","总分"])
+        for n in range(num):
+            u = ulist[n]
+            # if u[2] == "安徽":
+            csvWriter.writerow(u)
+            # time.sleep(0.01 * 5)
+            # print(tplt.format(u[0],u[1],u[2],u[3],chr(12288)))
 
-	with open('./tUniverList.csv','w+',newline='') as f: 
-		csvWriter = csv.writer(f)
-		csvWriter.writerow(["排名","大学名称","省市","排名积分"])
-		for n in range(num):
-			u = ulist[n]
-			# if u[2] == "安徽":
-			csvWriter.writerow(u)
-			# time.sleep(0.01*5)
-			# print(tplt.format(u[0],u[1],u[2],u[3],chr(12288)))
-
-def main():
-	unifo = []
-	url = "http://www.zuihaodaxue.cn/zuihaodaxuepaiming2018.html"
-	html = getHTML(url)
-	fillUnivrList(html,unifo)
-	printUniverList(unifo,600)
-main()
+if __name__=="__main__":
+    uinfo = []
+    url = "http://www.zuihaodaxue.cn/zuihaodaxuepaiming2018.html"
+    response = get_html(url)
+    fill_univrList_xpath(response, uinfo)
+    print_univerList(uinfo, 600)
