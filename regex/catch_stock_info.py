78568647caf32b04450ea943c5c3d84b978fea9b
Strli <18792146966@163.com>
2019-03-21 11:51:23 +0800

    update:更正代码规范

--- regex/catch_stock_info.py
+++ regex/catch_stock_info.py
@@ -6,8 +6,10 @@ import traceback
 import requests
 from bs4 import BeautifulSoup
 
-# 获取页面信息
+
 def get_html(url, code = 'utf-8'):
+    '''获取页面信息'''
+    
     try:
         rq = requests.get(url, timeout = 30)
         rq.raise_for_status()
@@ -16,20 +18,23 @@ def get_html(url, code = 'utf-8'):
     except:
         return "something wrong"
         
-# 获取股票列表，组成访问股票信息的url
 def get_stocklist(slist, htmlUrl):
+    '''获取股票列表，组成访问股票信息的url'''
+    
     html = get_html(htmlUrl, 'GB2312')
     soup = BeautifulSoup(html, "html.parser")
     a = soup.find_all("a")
     for i in a :
         try:
             href = i.attrs["href"]
-            slist.append(re.findall(r'[s][zh]\d{6}',href)[0]) #findall 返回列表类型,匹配不到则返回空列表，此时findall()[0] 出现指针异常
+            slist.append(re.findall(r'[s][zh]\d{6}', href)[0]) #findall 返回列表类型,匹配不到则返回空列表，此时findall()[0] 出现指针异常
         except: #增加Try..except是为了达到如下效果：href为空时，直接跳出循环；href不为空，但是不匹配regex时，直接调到下次循环...保证slist中不会出现空列表；
             continue
 
-# 访问股票列表代码网页，提取其中相关信息并存储到文件中
-def get_stockinfo(slist,stock_url,fpath):
+
+def get_stockinfo(slist, stock_url,fpath):
+    '''访问股票列表代码网页，提取其中相关信息并存储到文件中'''
+    
     count = 0
     for end_url in slist:
         # if end_url == "":
@@ -45,7 +50,7 @@ def get_stockinfo(slist,stock_url,fpath):
             if type(stockInfo) == None:
                 continue
             name = stockInfo.find_all('a', attrs={'class':'bets-name'})[0]
-            stock_info_dict.update({'股票名称': name.text.split()[0]})
+            stock_info_dict.update({'股票名称':name.text.split()[0]})
 
             keyList = stockInfo.find_all('dt')
             valueList = stockInfo.find_all('dd')
@@ -55,7 +60,7 @@ def get_stockinfo(slist,stock_url,fpath):
                 value = valueList[i].text
                 stock_info_dict[key] = value
                 
-            with open(fpath,'a',encoding = "utf-8") as f:
+            with open(fpath, 'a', encoding="utf-8") as f:
                 f.write(str(stock_info_dict) + '\n')
                 count += 1
                 print('\r当前进度：{:.2f}%'.format(count * 100 / len(slist)), end=' ')
@@ -65,7 +70,7 @@ def get_stockinfo(slist,stock_url,fpath):
             
 
 if __name__=="__main__":
-    '''123232'''
+    '''主函数'''
     
     slist = []
     output_path = "D:/stock_info.txt"
--- regex/catch_taobaoprice.py
+++ regex/catch_taobaoprice.py
@@ -2,10 +2,15 @@
 # -*-coding:utf:8 -*-
 
 import re
+import logging
+
 import requests
 
-# get data
-def getHtml(url):
+logging.basicConfig(level=logging.INFO)
+
+def get_html(url):
+    '''get data'''
+    
 	try:
 		rq = requests.get(url, timeout = 30)
 		rq.raise_for_status()
@@ -14,11 +19,12 @@ def getHtml(url):
 	except:
 		return "抓取失败"
 
-# handle data
-def parsePrice(ult, html):
+def parse_price(ult, html):
+    '''handle data'''
+    
 	try:
-		plt = re.findall(r'\"view_price\"\:\"[\d\.]*\"',html)  # \" 将字符串标识转义成字符，这样才能对内部字符进行检索
-		tlt = re.findall(r'\"raw_title\"\:\".*?\"',html)  # .* 可以匹配所有字符串，默认输出最大匹配字符串  .*? 输出最小匹配
+		plt = re.findall(r'\"view_price\"\:\"[\d\.]*\"', html)  # \" 将字符串标识转义成字符，这样才能对内部字符进行检索
+		tlt = re.findall(r'\"raw_title\"\:\".*?\"', html)  # .* 可以匹配所有字符串，默认输出最大匹配字符串  .*? 输出最小匹配
 		for i in range(len(plt)):   #java害人啊  len(plt)  写成了 plt.len()  mmp
 			price = eval(plt[i].split(':')[1])
 			title = eval(tlt[i].split(':')[1])
@@ -27,20 +33,25 @@ def parsePrice(ult, html):
 	except:
 		print("error")
         
-# format data
+
 def printGoodsList(ilt):
+    '''format data'''
+    
 	count = 0
 	tplt = "{0:^4}\t{1:^10}\t{2:{3}^16}"
-	print(tplt.format("序号","price","商品名称",chr(12288)))
+	print(tplt.format("序号", "price", "商品名称", chr(12288)))
 	for g in ilt:
 		count += 1
-		print(tplt.format(count,g[0],g[1],chr(12288)))
+		print(tplt.format(count, g[0], g[1], chr(12288)))
 
-def main():
+if __name__=="__main__":
+    '''
+    主函数：根据货物名称搜索，然后获取信息'''
+    
 	goods = "书包"
 	start_url = "https://s.taobao.com/search?q="
 	ilt = []
-	url = start_url + goods +"&s="
+	url = start_url + goods + "&s="
 	deepth = 3
 	for i in range(deepth):
 		try:
@@ -49,7 +60,6 @@ def main():
 			parsePrice(ilt,html)
 		except:
 			continue
-	# print(ilt)
 	printGoodsList(ilt)
 
 main()
